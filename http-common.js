a272e76d691819a136515f698420327188da3f7d
jkgenabe <jacklyngenabe@gmail.com>
2022-09-21 11:38:19 +0800

    Update API LINK

--- http-common.js
+++ http-common.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 //const _username = 'heeyRnrF)LZ%y#6XS{SS'
 //const _password = 'RK]j6=,4uV<G_zY$p6Ns'
 export const API = axios.create({
-  baseURL: `http://52.220.32.14:10113/api`,
+  //52.220.32.14:10113
+  baseURL: `http://198.19.226.41:10001/api`,
   headers: {
     'Content-Type': 'application/json',
   },
